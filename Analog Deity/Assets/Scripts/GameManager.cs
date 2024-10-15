4eec27d0725a18192e807b48222d0df00c488443
falconttu <k2155027@kingston.ac.uk>
2024-10-15 19:11:33 +0100

    Fixed display scaling and added lives and a death screen

--- Analog Deity/Assets/Scripts/GameManager.cs
+++ Analog Deity/Assets/Scripts/GameManager.cs
@@ -3,15 +3,28 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-
     public GameObject tryAgain_Panel;
     public GameObject correct_Panel;
+    
     public int xp = 0;
-    public TextMeshProUGUI xpText;
+    int Lives;
+
+    [SerializeField] TextMeshProUGUI xpText;
+    [SerializeField] TextMeshProUGUI LivesText;
+
+    [SerializeField] GameObject DeathScreen;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Lives = 3;
+
         xpText.text = "XP: " + xp;
+        LivesText.text = "Lives: " + Lives;
+
+        tryAgain_Panel.SetActive(false);
+        correct_Panel.SetActive(false);
+        DeathScreen.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,6 +36,13 @@ public class GameManager : MonoBehaviour
     public void TryAgain()
     {
         tryAgain_Panel.SetActive(false);
+        Lives--;
+        LivesText.text = "Lives: " + Lives;
+
+        if (Lives == 0)
+        {
+            DeathScreen.SetActive(true);
+        }
     }
 
     public void Correct()
